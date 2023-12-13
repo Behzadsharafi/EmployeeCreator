26a8c38fd5856789201d8337329a23f4430ac632
Behzad Sharafi <behzadsharafi@gmail.com>
2023-12-13 18:18:26 +1100

    adds a test for findAll method

---
+++ employee-creator-backend/src/test/java/com/behzad/employeecreatorbackend/EmployeeServiceTest.java
@@ -0,0 +1,33 @@
+package com.behzad.employeecreatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import com.behzad.employeecreatorbackend.employee.EmployeeRepository;
+import com.behzad.employeecreatorbackend.employee.EmployeeService;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeServiceTest {
+	
+	@Mock
+	private EmployeeRepository employeeRepository;
+	
+	@Mock
+	private ModelMapper mapper;
+	
+	@InjectMocks
+	private EmployeeService underTest;
+	
+	@Test
+	void findAll_ReturnsAllData() {
+		underTest.findAll();
+		Mockito.verify(employeeRepository).findAll();
+		
+	}
+
+}
